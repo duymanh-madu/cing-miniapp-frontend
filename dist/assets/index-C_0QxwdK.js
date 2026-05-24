4e292fe7b56126fb6e6a4bf44c3619931a3c8dd2
Nguyễn Duy Mạnh <style@MacBook-Pro-cua-Nguyen.local>
2026-05-24 20:09:03 +0700

    chore: remove dist from git tracking

--- dist/assets/index-C_0QxwdK.js
+++
@@ -1,1 +0,0 @@
-import{B as r}from"./BlackPearlRush-DyxZYvtq.js";import"./index-g9cGbQ-1.js";export{r as default};
--- dist/assets/index-DTVbN_Ec.js
+++
@@ -1,3 +0,0 @@
-import{j as n}from"./index-g9cGbQ-1.js";function e(){return n.jsx("div",{className:`
-        p-4
-      `,children:"Account Page"})}export{e as default};
