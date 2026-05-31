4c5211aad15ff112e69144c62c7daa2ec2d0a4ef
Nguyễn Duy Mạnh <style@MacBook-Pro-cua-Nguyen.local>
2026-06-01 00:08:48 +0700

    fix: đóng div tip picker

--- src/features/game-center/games/chess/ChessGame.jsx
+++ src/features/game-center/games/chess/ChessGame.jsx
@@ -1035,6 +1035,7 @@ export default function ChessGame({ onExit }) {
                 Huỷ
               </button>
             </div>
+          </div>
         )}
         {showChat && (
             <div style={{ position:"fixed", bottom:0, left:0, right:0, zIndex:9998,
